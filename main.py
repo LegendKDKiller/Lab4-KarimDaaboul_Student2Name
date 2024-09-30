c96c1cbdd0ea497d766b0e81233c06cea55f1022
Karim Daaboul <kmd21@mail.aub.edu>
2024-10-01 01:14:13 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from window import MainWindow  # Importing MainWindow class from window.py
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)  # Creating the application
-    main_window = MainWindow()  # Instantiating MainWindow
-    main_window.show()  # Showing the main window
-    sys.exit(app.exec_())
